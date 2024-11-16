0d1bfcd4bea7dab07351f6b06ead04921322cf70
Nitya Tripathi <diyatripathi70@gmail.com>
2024-11-16 20:17:30 +0530

    new commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 // add new feature - button  
 // merge conflicts 
 // an event that takes place when git is unable to automatticaly resolve diff in code btw two commits
-=======
+
 // add new feature - form
->>>>>>> feature
+
